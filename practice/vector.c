c0d874b99b49456588dc3c4543201623e0b196e6
JongeunKeum <jon9eun@gmail.com>
2021-01-15 15:40:24 +0100

    vector utils

---
+++ practice/vector.c
@@ -0,0 +1,101 @@
+#include <mlx.h>
+#include <stdio.h>
+#include <math.h>
+
+typedef struct	s_vector
+{
+	double	x;
+	double	y;
+	double	z;
+}				t_vector;
+
+t_vector		vector_ctor(double x, double y, double z)
+{
+	t_vector	v;
+
+	v.x = x;
+	v.y = y;
+	v.z = z;
+	return (v);
+}
+
+t_vector		v_add(t_vector v1, t_vector v2)
+{
+	t_vector	r;
+
+	r.x = v1.x + v2.x;
+	r.y = v1.y + v2.y;
+	r.z = v1.z + v2.z;
+	return (r);
+}
+
+t_vector		v_sub(t_vector v1, t_vector v2)
+{
+	t_vector	r;
+
+	r.x = v1.x - v2.x;
+	r.y = v1.y - v2.y;
+	r.z = v1.z - v2.z;
+	return (r);
+}
+
+t_vector		v_mul(t_vector v1, t_vector v2)
+{
+	t_vector	r;
+
+	r.x = v1.x * v2.x;
+	r.y = v1.y * v2.y;
+	r.z = v1.z * v2.z;
+	return (r);
+}
+
+t_vector		n_mul(t_vector v, int n)
+{
+	t_vector	r;
+
+	r.x = v.x * n;
+	r.y = v.y * n;
+	r.z = v.z * n;
+	return (r);
+}
+
+t_vector		n_div(t_vector v, int n)
+{
+	t_vector	r;
+
+	r.x = v.x / n;
+	r.y = v.y / n;
+	r.z = v.z / n;
+	return (r);
+}
+
+double			v_dot(t_vector v1, t_vector v2)
+{
+	double	result;
+
+	result = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+	return (result);
+}
+
+t_vector		v_cross(t_vector v1, t_vector v2)
+{
+	t_vector	r;
+
+	r.x = v1.y * v2.z - v1.z * v2.y;
+	r.y = v1.z * v2.x - v1.x * v2.z;
+	r.z = v1.x * v2.y - v1.y * v2.x;
+	return (r);
+}
+
+int				main(void)
+{
+	t_vector	v1;
+	t_vector	v2;
+	t_vector	v_sum;
+
+	v1 = vector_ctor(1.0, 2.0, 3.0);
+	v2 = vector_ctor(3.5, 2.5, 1.5);
+	v_sum = vector_add(v1, v2);
+	printf("x:%f\ny:%f\nz:%f\n", v_sum.x, v_sum.y, v_sum.z);
+	return (0);
+}
