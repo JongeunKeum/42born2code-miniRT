7b21b3b451e60fbf2b6fb4b178ee4e51e6727797
JongeunKeum <jon9eun@gmail.com>
2021-01-15 10:01:16 +0100

    draw circle & show gradation

--- practice/tutorial.c
+++ practice/tutorial.c
@@ -1,4 +1,5 @@
 #include <mlx.h>
+#include <math.h>
 
 typedef struct	s_data
 {
@@ -22,25 +23,25 @@ int 			main(void)
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
-	int		i;
-	int		j;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	mlx_win = mlx_new_window(mlx, 1920, 914, "Hello World!");
+	img.img = mlx_new_image(mlx, 1920, 914);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	/* square 
-	i = -1;
+	/* square */
+	/*
+	int i = -1;
 	while (++i < 100)
 	{
-		j = -1;
+		int j = -1;
 		while (++j < 100)
 			my_mlx_pixel_put(&img, i, j, 0x00FF0000);
 	}
 	*/
 	/* triangle */
-	i = 0;
-	j = 100;
+	/*
+	int i = 0;
+	int j = 100;
 	while (i < 100 && j > 0)
 	{
 		int k = i + 2 * j;
@@ -52,7 +53,40 @@ int 			main(void)
 		i++;
 		j--;
 	}
-	/**/
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 300, 300);
+	*/
+	/* circle */
+	/*
+	int	rad = 100;
+	int	i = -101;
+	int	point;
+	while (++i < 100)
+	{
+		int	j = -101;
+		while (++j < 100)
+		{
+			point = pow(i, 2) + pow(j, 2);
+			if (sqrt(point) <= rad)
+				my_mlx_pixel_put(&img, i + 100, j + 100, 0x00FF0000);
+		}
+	}
+	*/
+	/* gradation */
+	int	r = 0;
+	int	b = 0;
+	int	color = 0;
+	double	i = -1;
+	while (++i < 1920)
+	{
+		double	j = 914;
+		int	g = 0;
+		while (--j > 0)
+		{
+			color = r << 16 | g << 8 | b;
+			my_mlx_pixel_put(&img, i, 914 - j, color);
+			g = j / 914 * 256;
+		}
+		r = i / 1920 * 256;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
