b895802a3c344180effdda41fc16ed2e26d95e3c
JongeunKeum <jon9eun@gmail.com>
2021-01-20 08:50:53 +0100

    Sphere - hit_record include files

--- practice/include/structures.h
+++ practice/include/structures.h
@@ -6,6 +6,7 @@ typedef struct	s_vec3	t_vec3;
 typedef struct	s_vec3	t_point3;
 typedef struct	s_vec3	t_color3;
 typedef struct	s_ray	t_ray;
+typedef struct	s_hit_record	t_hit_record;
 
 /*	scene structure	*/
 typedef struct	s_camera	t_camera;
@@ -31,6 +32,16 @@ struct	s_ray
 {
 	t_point3	orig;
 	t_vec3		dir;
+};
+
+struct	s_hit_record
+{
+	t_point3	p;
+	t_vec3		normal;
+	double		tmin;
+	double		tmax;
+	double		t;
+	t_bool		front_face;
 };
 
 /*	scene structure	*/
