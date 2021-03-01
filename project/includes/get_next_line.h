1caf11b16c2aa629f80f8f4b5699b612be8aa978
JongeunKeum <jon9eun@gmail.com>
2021-03-01 20:55:03 +0900

    parsing rt file

---
+++ project/includes/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/22 16:40:03 by jkeum             #+#    #+#             */
+/*   Updated: 2021/03/01 19:15:27 by jkeum            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 3
+#endif
+
+int		get_next_line(int fd, char **line);
+int		ft_strlen(char *str);
+char	*ft_strjoin_gnl(char *save, char *buff);
+
+#endif
---
+++ project/includes/minirt.h
@@ -0,0 +1,9 @@
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include "libft.h"
+# include "get_next_line.h"
+
+#endif
