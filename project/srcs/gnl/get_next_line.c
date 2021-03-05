05ca3c5dab03730a19ab2507892b8396cc91a682
JongeunKeum <jon9eun@gmail.com>
2021-03-05 09:21:20 +0100

    gnl modified

--- project/srcs/gnl/get_next_line.c
+++ project/srcs/gnl/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "minirt.h"
 
 int		is_newline(char *save)
 {
--- project/srcs/gnl/get_next_line_utils.c
+++ project/srcs/gnl/get_next_line_utils.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "minirt.h"
 
 int		ft_strlen_gnl(char *s)
 {
