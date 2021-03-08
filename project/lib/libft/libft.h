ac8e2630c801e718d8dca9d60d49b17e7a6a451e
JongeunKeum <jon9eun@gmail.com>
2021-03-08 10:37:00 +0100

    ft_strrev deleted

--- project/lib/libft/libft.h
+++ project/lib/libft/libft.h
@@ -39,7 +39,6 @@ char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strnstr(const char *big,
 		const char *little, size_t len);
-char				*ft_strrev(char *str);
 void				*ft_memset(void *s, int c, size_t n);
 void				ft_bzero(void *s, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
--- project/srcs/main.c
+++ project/srcs/main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	t_vars	vars;
 
 	vars.scene = scene_init();
-	if (argc == 2 && ft_strncmp(ft_strrev(argv[1]), "tr.", 3))
+	if (argc == 2)
 	{
 		parse_rt(argv[1], vars.scene);
 		set_mlx(&vars);
@@ -25,13 +25,10 @@ int main(int argc, char *argv[])
 		handle_hook(&vars);
 		mlx_loop(vars.mlx);
 	}
-	else if (argc == 3 && ft_strcmp(argv[1], "--save"))
+	else if (argc == 3 && !ft_strcmp(argv[1], "--save"))
 	{
-		if (ft_strncmp(ft_strrev(argv[2]), "tr.", 3))
-		{
-			parse_rt(argv[2], vars.scene);
-			rendering_bmp(vars);
-		}
+		parse_rt(argv[2], vars.scene);
+		rendering_bmp(vars);
 	}
 //	else
 //		err;
