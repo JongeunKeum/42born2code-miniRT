3dad278be2b32d7e21d4b771d3dc5ef51aba56fc
JongeunKeum <jon9eun@gmail.com>
2021-03-08 10:06:28 +0100

    add new files in libft

--- project/lib/libft/libft.h
+++ project/lib/libft/libft.h
@@ -22,7 +22,6 @@ typedef struct		s_list
 	struct s_list	*next;
 }					t_list;
 
-double				ft_atod(char *str);
 int					ft_issign(char c);
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
@@ -31,6 +30,7 @@ int					ft_isascii(int c);
 int					ft_isprint(int c);
 int					ft_toupper(int c);
 int					ft_tolower(int c);
+int					ft_strcmp(char *s1, char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t				ft_strlen(const char *s);
 size_t				ft_strlcpy(char *dst, const char *src, size_t size);
@@ -39,6 +39,7 @@ char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strnstr(const char *big,
 		const char *little, size_t len);
+char				*ft_strrev(char *str);
 void				*ft_memset(void *s, int c, size_t n);
 void				ft_bzero(void *s, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
@@ -47,6 +48,7 @@ void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 int					ft_atoi(const char *str);
+double				ft_atod(char *str);
 void				*ft_calloc(size_t count, size_t size);
 char				*ft_strdup(const char *s1);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
